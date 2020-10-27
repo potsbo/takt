0174518f6d264aafdc89ac27a52c7a9752b227ef
Shimpei Otsubo <potsbo@gmail.com>
2020-10-27 17:54:54 +0900

    Refactor

--- cmd/takt/cmd.go
+++ cmd/takt/cmd.go
@@ -17,75 +17,92 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+type runFunc func(ctx context.Context) error
+
 func newCmd(ioset *clib.IO) *cobra.Command {
-	taktfilePath := ".takt.yaml"
+	runner := taktRunner{
+		ioset:        ioset,
+		taktfilePath: ".takt.yaml",
+	}
+
 	cmd := &cobra.Command{
 		Use:   "takt",
 		Short: "Task runner with cancel",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			runner := func(ctx context.Context) error {
-				file, err := os.Open(taktfilePath)
-				if err != nil {
-					return errors.Wrap(err, "failed to open file")
-				}
-				defer file.Close()
-
-				takt, err := task.Parse(file)
-				if err != nil {
-					return err
-				}
-
-				tasks, err := task.FromTakt(*takt)
-				if err != nil {
-					return err
-				}
-
-				eg, ctx := errgroup.WithContext(ctx)
-
-				logger := log.New(ioset.Out, "", log.Ldate|log.Ltime)
-				for _, t := range tasks {
-					t := t
-					eg.Go(func() error {
-						prefixLogger := logstreamer.NewLogstreamer(logger, fmt.Sprintf("%s: ", t.Name), false)
-						defer prefixLogger.Close()
-
-						return t.Run(ctx, prefixLogger)
-					})
-				}
-
-				return eg.Wait()
-			}
-
-			{
-				ctx, cancel := context.WithCancel(context.Background())
-				defer cancel()
-
-				cancelReceived := make(chan os.Signal, 1)
-				signal.Notify(cancelReceived, os.Interrupt, syscall.SIGTERM, os.Kill)
-				clean := func() {
-					<-cancelReceived
-					fmt.Println("Interrupted, cleaning up...")
-					cancel()
-				}
-				go clean()
-
-				err := runner(ctx)
-				close(cancelReceived)
-
-				if es, ok := err.(*execx.ExitStatus); ok {
-					if es.Signaled {
-						return nil
-					}
-				}
-				return errors.Wrap(err, "runner exited with error")
-			}
+			ctx := context.Background()
+			return wrapInterrupt(runner.Run)(ctx)
 		},
 	}
 
-	cmd.Flags().StringVarP(&taktfilePath, "file", "f", taktfilePath, "path to TaktFile")
+	cmd.Flags().StringVarP(&runner.taktfilePath, "file", "f", runner.taktfilePath, "path to TaktFile")
+
 	cmd.SetOut(ioset.Out)
 	cmd.SetErr(ioset.Err)
 	cmd.SetIn(ioset.In)
 
 	return cmd
+}
+
+func wrapInterrupt(runner runFunc) runFunc {
+	return func(ctx context.Context) error {
+		ctx, cancel := context.WithCancel(ctx)
+		defer cancel()
+
+		cancelReceived := make(chan os.Signal, 1)
+		signal.Notify(cancelReceived, os.Interrupt, syscall.SIGTERM, os.Kill)
+		clean := func() {
+			<-cancelReceived
+			fmt.Println("Interrupted, cleaning up...")
+			cancel()
+		}
+		go clean()
+
+		err := runner(ctx)
+		close(cancelReceived)
+
+		if es, ok := err.(*execx.ExitStatus); ok {
+			if es.Signaled {
+				return nil
+			}
+		}
+		return errors.Wrap(err, "runner exited with error")
+	}
+}
+
+type taktRunner struct {
+	taktfilePath string
+	ioset        *clib.IO
+}
+
+func (t taktRunner) Run(ctx context.Context) error {
+	file, err := os.Open(t.taktfilePath)
+	if err != nil {
+		return errors.Wrap(err, "failed to open file")
+	}
+	defer file.Close()
+
+	takt, err := task.Parse(file)
+	if err != nil {
+		return err
+	}
+
+	tasks, err := task.FromTakt(*takt)
+	if err != nil {
+		return err
+	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	logger := log.New(t.ioset.Out, "", log.Ldate|log.Ltime)
+	for _, t := range tasks {
+		t := t
+		eg.Go(func() error {
+			prefixLogger := logstreamer.NewLogstreamer(logger, fmt.Sprintf("%s: ", t.Name), false)
+			defer prefixLogger.Close()
+
+			return t.Run(ctx, prefixLogger)
+		})
+	}
+
+	return eg.Wait()
 }
