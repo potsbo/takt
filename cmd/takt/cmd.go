4c4d50f9e78232f4cd268c9c0ea0c8e43a12d51c
Shimpei Otsubo <potsbo@gmail.com>
2020-10-27 15:22:28 +0900

    Accept sigterm

--- cmd/takt/cmd.go
+++ cmd/takt/cmd.go
@@ -5,7 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"time"
+	"os/signal"
+	"syscall"
 
 	"github.com/izumin5210/clig/pkg/clib"
 	"github.com/kvz/logstreamer"
@@ -21,47 +22,68 @@ func newCmd(ioset *clib.IO) *cobra.Command {
 		Use:   "takt",
 		Short: "Task runner with cancel",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			file, err := os.Open(taktfilePath)
-			if err != nil {
-				return errors.Wrap(err, "failed to open file")
-			}
-			defer file.Close()
-
-			takt, err := task.Parse(file)
-			if err != nil {
-				return err
-			}
-
-			tasks, err := task.FromTakt(*takt)
-			if err != nil {
-				return err
-			}
-
-			ctx, cancel := context.WithTimeout(context.Background(), 50*time.Second)
+			ctx, cancel := context.WithCancel(context.Background())
 			defer cancel()
 
-			eg := errgroup.Group{}
+			runner := func(ctx context.Context) error {
+				file, err := os.Open(taktfilePath)
+				if err != nil {
+					return errors.Wrap(err, "failed to open file")
+				}
+				defer file.Close()
 
-			logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-			for _, t := range tasks {
-				t := t
-				eg.Go(func() error {
-					prefixLogger := logstreamer.NewLogstreamer(logger, fmt.Sprintf("%s: ", t.Name), false)
-					defer prefixLogger.Close()
+				takt, err := task.Parse(file)
+				if err != nil {
+					return err
+				}
 
-					if err := t.Run(ctx, prefixLogger); err != nil {
-						cancel()
-						return err
-					}
+				tasks, err := task.FromTakt(*takt)
+				if err != nil {
+					return err
+				}
 
-					return nil
-				})
+				eg := errgroup.Group{}
+
+				logger := log.New(ioset.Out, "", log.Ldate|log.Ltime)
+				for _, t := range tasks {
+					t := t
+					eg.Go(func() error {
+						prefixLogger := logstreamer.NewLogstreamer(logger, fmt.Sprintf("%s: ", t.Name), false)
+						defer prefixLogger.Close()
+
+						if err := t.Run(ctx, prefixLogger); err != nil {
+							cancel()
+							return err
+						}
+
+						return nil
+					})
+				}
+
+				if err := eg.Wait(); err != nil {
+					return err
+				}
+
+				return nil
 			}
 
-			if err := eg.Wait(); err != nil {
-				return err
+			{
+				done := make(chan bool, 1)
+
+				c := make(chan os.Signal, 1)
+				signal.Notify(c, os.Interrupt, syscall.SIGTERM, os.Kill)
+				go func() {
+					<-c
+					fmt.Println("Interrupted, cleaning up...")
+					cancel()
+					done <- true
+				}()
+
+				err := runner(ctx)
+				close(c)
+				<-done
+				return errors.Wrap(err, "failed to run")
 			}
-			return nil
 		},
 	}
 
